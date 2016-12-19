53488140c7700f39058361cadbb23cbae1e26848
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-19 22:01:55 +0530

    Simple is Goooooood!

--- app/src/main/java/com/appbusters/robinkamboj/gallery/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/gallery/MainActivity.java
@@ -2,6 +2,7 @@ package com.appbusters.robinkamboj.gallery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
