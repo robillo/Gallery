ecfd69ef3bef1606c5d4bc907b7a62bbd739f81f
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-19 23:21:05 +0530

    Mediastore image intent

--- app/src/main/java/com/appbusters/robinkamboj/gallery/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/gallery/MainActivity.java
@@ -1,14 +1,24 @@
 package com.appbusters.robinkamboj.gallery;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int RESULT_LOAD_IMAGE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
     }
 }
