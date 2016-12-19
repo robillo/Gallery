7ae71d6bb994ecc42d580baaaf3416ab1c714960
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-20 00:01:24 +0530

    working, but image not showing in IV (maybe read ext. stg. perm. is reqd.)

--- app/src/main/java/com/appbusters/robinkamboj/gallery/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/gallery/MainActivity.java
@@ -1,11 +1,15 @@
 package com.appbusters.robinkamboj.gallery;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View view) {
+    protected void onClick(View view) {
         Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode== RESULT_LOAD_IMAGE && resultCode==RESULT_OK && data!= null){
+            Uri selectedImage= data.getData();
+            String[] filePathColumn= { MediaStore.Images.Media.DATA};
+
+            Cursor cursor = getContentResolver().query( selectedImage, filePathColumn, null, null, null);
+            assert cursor != null;
+            cursor.moveToFirst();
+
+            int columnIndex= cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath= cursor.getString(columnIndex);
+            cursor.close();
+
+            //String picturepath contains the path of the selected image.
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
     }
 }
