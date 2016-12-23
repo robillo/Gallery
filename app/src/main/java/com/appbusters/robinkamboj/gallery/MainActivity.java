3013637f304a52366a67c9ea0173e3a5a10fa656
Robin Kamboj <dev.robinkamboj@gmail.com>
2016-12-23 07:51:36 +0530

    AUTO CAPS on edittexts.

--- app/src/main/java/com/appbusters/robinkamboj/gallery/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/gallery/MainActivity.java
@@ -12,6 +12,7 @@ import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -29,6 +30,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText title= (EditText) findViewById(R.id.title);
+        EditText subTitle= (EditText) findViewById(R.id.subTitle);
+        //Auto Capitalize all letters of EditTexts
+        title.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+        subTitle.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
 
         String[] permissions={Manifest.permission.READ_EXTERNAL_STORAGE};
 
